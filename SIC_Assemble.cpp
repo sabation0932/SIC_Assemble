eccb4a431340a06b0c7ebd7330179e4ab051ff96
sabation0932 <sabation0932@gmail.com>
2022-05-23 21:24:44 +0800

    加入 source_file class

--- SIC_Assemble.cpp
+++ SIC_Assemble.cpp
@@ -3,8 +3,27 @@
 #include <fstream>
 #include <string>
 
+#define SOURCE_PATH "../110_Assemble_FinalProject/source.txt"
+#define OPCODE_PATH "../110_Assemble_FinalProject/opcode.txt"
+
 using namespace std;
 
-int  main(){
-    cout<<"Hello Wolrd"<<endl;
+class source_file
+{
+private:
+    fstream source;
+
+public:
+    void load_data(void)
+    {
+        cout << "load_data" << endl;
+    }
+};
+
+int main()
+{
+    cout << "--start--" << endl;
+    source_file source;
+    source.load_data();
+    cout << "--End--" << endl;
 }
