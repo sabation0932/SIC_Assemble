e7b35d94ec4887b8888286b248de475ae0eb372f
sabation0932 <sabation0932@gmail.com>
2022-05-23 22:02:16 +0800

    完成檔案開啟

--- SIC_Assemble.cpp
+++ SIC_Assemble.cpp
@@ -8,6 +8,26 @@
 #define OPCODE_PATH "../110_Assemble_FinalProject/opcode.txt"
 
 using namespace std;
+class file_operate
+{
+private:
+    fstream file;
+    string path;
+
+public:
+    void load_file(fstream _file, string _path)
+    {
+        file.open(path, ios::in);
+        if (file.is_open())
+        {
+            string line;
+            while (getline(file, line))
+            {
+                cout << line << endl;
+            }
+        }
+    }
+};
 
 class source_file
 {
@@ -17,14 +37,42 @@ private:
 public:
     void load_data(void)
     {
-        source.open(SOURCE_PATH,ios::in);
-        if(source.is_open()){
+        source.open(SOURCE_PATH, ios::in);
+        if (source.is_open())
+        {
             string line;
-            while (getline(source,line))
+            while (getline(source, line))
             {
-                cout<<line<<endl;
+                cout << line << endl;
             }
-            
+        }
+        else
+        {
+            cout << "here is no source file" << endl;
+        }
+    }
+};
+
+class opcode_file
+{
+private:
+    fstream opcode;
+
+public:
+    void load_data(void)
+    {
+        opcode.open(OPCODE_PATH, ios::in);
+        if (opcode.is_open())
+        {
+            string line;
+            while (getline(opcode, line))
+            {
+                cout << line << endl;
+            }
+        }
+        else
+        {
+            cout << "here is no opcode file" << endl;
         }
     }
 };
@@ -34,5 +82,8 @@ int main()
     cout << "--start--" << endl;
     source_file source;
     source.load_data();
+    cout << "------" << endl;
+    opcode_file opcode;
+    opcode.load_data();
     cout << "--End--" << endl;
 }
